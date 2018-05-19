c8edff192048dc29d5ca1bf541b04d2fc9108541
Robert Laszczak <roblaszczak@gmail.com>
2018-05-20 00:33:38 +0200

    added throttle middleware

---
+++ message/handler/middleware/throttle.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"github.com/roblaszczak/gooddd/message/handler"
+	"github.com/roblaszczak/gooddd/message"
+	"github.com/throttled/throttled"
+	"time"
+	"github.com/roblaszczak/gooddd"
+	"github.com/throttled/throttled/store/memstore"
+	"github.com/pkg/errors"
+)
+
+type Throttle struct {
+	rateLimiter throttled.RateLimiter
+	logger      gooddd.LoggerAdapter
+}
+
+func NewThrottlePerSecond(perSecond int, logger gooddd.LoggerAdapter) (Throttle, error) {
+	store, err := memstore.New(1)
+	if err != nil {
+		return Throttle{}, errors.Wrap(err, "cannot create throttle storage")
+	}
+
+	quota := throttled.RateQuota{throttled.PerSec(perSecond), 0}
+	rateLimiter, err := throttled.NewGCRARateLimiter(store, quota)
+	if err != nil {
+		return Throttle{}, errors.Wrap(err, "cannot initialize rate limiter")
+	}
+
+	return Throttle{rateLimiter, logger}, nil
+}
+
+func (t Throttle) Middleware(h handler.HandlerFunc) handler.HandlerFunc {
+	return func(message *message.Message) ([]message.Payload, error) {
+		defer func() {
+			limited, context, err := t.rateLimiter.RateLimit("", 1)
+			if err != nil {
+				t.logger.Info("Throttle error", gooddd.LogFields{"err": err.Error()})
+			}
+			if limited {
+				t.logger.Info("Throttling", gooddd.LogFields{"wait_duration": context.RetryAfter.String()})
+				time.Sleep(context.RetryAfter)
+			}
+		}()
+
+		return h(message)
+	}
+}
