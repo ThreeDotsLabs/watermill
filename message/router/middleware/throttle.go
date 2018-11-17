5b547043046b0d88421a83a8042d2cc2a573b656
Maciej Bratek <maclav3@gmail.com>
2018-11-17 18:37:05 +0100

    Add a very simple throttle middleware with unit test

--- message/router/middleware/throttle.go
+++ message/router/middleware/throttle.go
@@ -1,46 +1,24 @@
 package middleware
 
 import (
-	"time"
-
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
-	"github.com/pkg/errors"
-	"github.com/throttled/throttled"
-	"github.com/throttled/throttled/store/memstore"
+	"time"
 )
 
 type Throttle struct {
-	rateLimiter throttled.RateLimiter
+	perSecond int
 	logger      watermill.LoggerAdapter
 }
 
 func NewThrottlePerSecond(perSecond int, logger watermill.LoggerAdapter) (Throttle, error) {
-	store, err := memstore.New(1)
-	if err != nil {
-		return Throttle{}, errors.Wrap(err, "cannot create throttle storage")
-	}
-
-	quota := throttled.RateQuota{throttled.PerSec(perSecond), 0}
-	rateLimiter, err := throttled.NewGCRARateLimiter(store, quota)
-	if err != nil {
-		return Throttle{}, errors.Wrap(err, "cannot initialize rate limiter")
-	}
-
-	return Throttle{rateLimiter, logger}, nil
+	return Throttle{perSecond, logger}, nil
 }
 
 func (t Throttle) Middleware(h message.HandlerFunc) message.HandlerFunc {
 	return func(message *message.Message) ([]*message.Message, error) {
 		defer func() {
-			limited, context, err := t.rateLimiter.RateLimit("", 1)
-			if err != nil {
-				t.logger.Info("Throttle error", watermill.LogFields{"err": err.Error()})
-			}
-			if limited {
-				t.logger.Info("Throttling", watermill.LogFields{"wait_duration": context.RetryAfter.String()})
-				time.Sleep(context.RetryAfter)
-			}
+			time.Sleep(time.Duration(int(time.Second) / t.perSecond))
 		}()
 
 		return h(message)
---
+++ message/router/middleware/throttle_test.go
@@ -0,0 +1,61 @@
+package middleware_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/message/router/middleware"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ThreeDotsLabs/watermill/message"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	perSecond   = 10
+	testTimeout = time.Second
+)
+
+func TestThrottle_Middleware(t *testing.T) {
+	throttle, err := middleware.NewThrottlePerSecond(perSecond, watermill.NewStdLogger(true, true))
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+
+	producedMessagesCounter := 0
+	productionDone := false
+	for {
+		produced, err := throttle.Middleware(handlerFuncAlwaysOK)(
+			message.NewMessage("uuid", nil),
+		)
+		if err != nil {
+			cancel()
+			t.Fail()
+			return
+		}
+
+		assert.NoError(t, err)
+		assert.Equal(t, handlerFuncAlwaysOKMessages, produced)
+		producedMessagesCounter++
+
+		select {
+		case <-ctx.Done():
+			productionDone = true
+		default:
+		}
+
+		if productionDone {
+			break
+		}
+	}
+
+	t.Logf("produced %d messages in %d seconds, at rate of total %d messages per second",
+		producedMessagesCounter,
+		int(testTimeout.Seconds()),
+		perSecond,
+	)
+	assert.True(t, producedMessagesCounter <= int(perSecond*testTimeout.Seconds()))
+}
