4a14c094918501588f7a67f80596c4b94a813f37
Maciej Bratek <maclav3@gmail.com>
2018-11-30 15:47:41 +0100

    Remove obsolete test; Implement Unmarshaler properly; move docker-compose into a more fitting place

--- message/infrastructure/googlecloud/marshaler.go
+++ message/infrastructure/googlecloud/marshaler.go
@@ -20,6 +20,11 @@ const UUIDHeaderKey = "_watermill_message_uuid"
 type DefaultMarshaler struct{}
 type DefaultUnmarshaler struct{}
 
+type MarshalerUnmarshaler struct {
+	DefaultMarshaler
+	DefaultUnmarshaler
+}
+
 func (m DefaultMarshaler) Marshal(topic string, msg *message.Message) (*pubsub.Message, error) {
 	if value := msg.Metadata.Get(UUIDHeaderKey); value != "" {
 		return nil, errors.Errorf("metadata %s is reserved by watermill for message UUID", UUIDHeaderKey)
@@ -41,6 +46,17 @@ func (m DefaultMarshaler) Marshal(topic string, msg *message.Message) (*pubsub.M
 	return marshaledMsg, nil
 }
 
-func (u DefaultUnmarshaler) Unmarshal(*pubsub.Message) (*message.Message, error) {
-	panic("implement me")
+func (u DefaultUnmarshaler) Unmarshal(pubsubMsg *pubsub.Message) (*message.Message, error) {
+	metadata := make(message.Metadata, len(pubsubMsg.Attributes))
+
+	for k, attr := range pubsubMsg.Attributes {
+		metadata.Set(k, attr)
+	}
+
+	metadata.Set("publishTime", pubsubMsg.PublishTime.String())
+
+	msg := message.NewMessage(pubsubMsg.ID, pubsubMsg.Data)
+	msg.Metadata = metadata
+
+	return msg, nil
 }
