405c6189b0ad0b80108c980dfe1f2a2bea69f151
Robert Laszczak <roblaszczak@gmail.com>
2018-12-05 14:48:34 +0100

    fix netlify configs

--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,12 @@
+set -e -x
+
 if [ ! -d themes/kube ]; then
-    git clone https://github.com/jeblister/kube themes/kube || true
-    git checkout 5f68bf3e990eff4108fa251f3a3112d081fffba4
+    mkdir -p themes/kube && pushd themes/kube
+    git init
+    git remote add origin https://github.com/jeblister/kube
+    git fetch --depth 1 origin 5f68bf3e990eff4108fa251f3a3112d081fffba4
+    git checkout FETCH_HEAD
+    popd
 fi
 
 hugo --gc --minify
