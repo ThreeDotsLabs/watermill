6f3f397e95d2e27e4e7f3cc621ed31193cf23694
Robert Laszczak <roblaszczak@gmail.com>
2018-12-11 00:02:30 +0100

    [godoc] added watermill.io info

---
+++ doc.go
@@ -0,0 +1,13 @@
+// Watermill is a Golang library for working efficiently with message streams.
+//
+// It is intended for building event driven applications,
+// enabling event sourcing, RPC over messages, sagas
+// and basically whatever else comes to your mind.
+//
+// You can use conventional pub/sub implementations
+// like Kafka or RabbitMQ, but also HTTP or MySQL binlog if that fits your use case.
+//
+// Website with detailed documentation: https://watermill.io/
+//
+// Getting started guide: https://watermill.io/docs/getting-started/
+package watermill
