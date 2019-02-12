cb0b3af09d83051ffdb5bdd5a0509e0ed01406f2
Maciej Bratek <maclav3@gmail.com>
2019-02-12 20:36:22 +0100

    Add validate example config to metrics example

--- _examples/metrics/.validate_example.sh
+++ _examples/metrics/.validate_example.sh
@@ -2,7 +2,7 @@
 #
 # how to run the command
 CMD="docker-compose up"
-# how long to wait for the output 
-TIMEOUT=5
+# how long to wait for the output
+TIMEOUT=8
 # we expect this regexp to be present in the output
-EXPECTED_OUTPUT="msg=\"Received message\""
+EXPECTED_OUTPUT="msg=\"Message acked\""
