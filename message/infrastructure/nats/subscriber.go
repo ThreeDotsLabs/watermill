44f4c8684bd316fb97317b35f6da8d4581e686db
MartinForReal <fanshangxiang@gmail.com>
2019-03-15 04:07:32 +0800

    add function for subscriber/publisher to reuse nats streaming connection (#58)

--- message/infrastructure/nats/subscriber.go
+++ message/infrastructure/nats/subscriber.go
@@ -74,6 +74,62 @@ type StreamingSubscriberConfig struct {
 	Unmarshaler Unmarshaler
 }
 
+type StreamingSubscriberSubscriptionConfig struct {
+
+	// StanSubscriptionOptions are custom []stan.SubscriptionOption passed to subscription.
+	StanSubscriptionOptions []stan.SubscriptionOption
+
+	// Unmarshaler is an unmarshaler used to unmarshaling messages from NATS format to Watermill format.
+	Unmarshaler Unmarshaler
+	// QueueGroup is the NATS Streaming queue group.
+	//
+	// All subscriptions with the same queue name (regardless of the connection they originate from)
+	// will form a queue group. Each message will be delivered to only one subscriber per queue group,
+	// using queuing semantics.
+	//
+	// It is recommended to set it with DurableName.
+	// For non durable queue subscribers, when the last member leaves the group,
+	// that group is removed. A durable queue group (DurableName) allows you to have all members leave
+	// but still maintain state. When a member re-joins, it starts at the last position in that group.
+	//
+	// When QueueGroup is empty, subscribe without QueueGroup will be used.
+	QueueGroup string
+
+	// DurableName is the NATS streaming durable name.
+	//
+	// Subscriptions may also specify a “durable name” which will survive client restarts.
+	// Durable subscriptions cause the server to track the last acknowledged message
+	// sequence number for a client and durable name. When the client restarts/resubscribes,
+	// and uses the same client ID and durable name, the server will resume delivery beginning
+	// with the earliest unacknowledged message for this durable subscription.
+	//
+	// Doing this causes the NATS Streaming server to track
+	// the last acknowledged message for that ClientID + DurableName.
+	DurableName string
+
+	// SubscribersCount determines wow much concurrent subscribers should be started.
+	SubscribersCount int
+
+	// How long subscriber should wait for Ack/Nack. When no Ack/Nack was received, message will be redelivered.
+	// It is mapped to stan.AckWait option.
+	AckWaitTimeout time.Duration
+	// CloseTimeout determines how long subscriber will wait for Ack/Nack on close.
+	// When no Ack/Nack is received after CloseTimeout, subscriber will be closed.
+	CloseTimeout time.Duration
+}
+
+func (c *StreamingSubscriberConfig) GetStreamingSubscriberSubscriptionConfig() StreamingSubscriberSubscriptionConfig {
+	return StreamingSubscriberSubscriptionConfig{
+		StanSubscriptionOptions: c.StanSubscriptionOptions,
+		Unmarshaler:             c.Unmarshaler,
+		QueueGroup:              c.QueueGroup,
+		DurableName:             c.DurableName,
+		SubscribersCount:        c.SubscribersCount,
+		AckWaitTimeout:          c.AckWaitTimeout,
+		CloseTimeout:            c.CloseTimeout,
+	}
+}
+
 func (c *StreamingSubscriberConfig) setDefaults() {
 	if c.SubscribersCount <= 0 {
 		c.SubscribersCount = 1
@@ -96,7 +152,7 @@ func (c *StreamingSubscriberConfig) setDefaults() {
 	}
 }
 
-func (c *StreamingSubscriberConfig) Validate() error {
+func (c *StreamingSubscriberSubscriptionConfig) Validate() error {
 	if c.Unmarshaler == nil {
 		return errors.New("StreamingSubscriberConfig.Unmarshaler is missing")
 	}
@@ -116,7 +172,7 @@ type StreamingSubscriber struct {
 	conn   stan.Conn
 	logger watermill.LoggerAdapter
 
-	config StreamingSubscriberConfig
+	config StreamingSubscriberSubscriptionConfig
 
 	subs     []stan.Subscription
 	subsLock sync.Mutex
@@ -139,15 +195,17 @@ type StreamingSubscriber struct {
 func NewStreamingSubscriber(config StreamingSubscriberConfig, logger watermill.LoggerAdapter) (*StreamingSubscriber, error) {
 	config.setDefaults()
 
-	if err := config.Validate(); err != nil {
-		return nil, err
-	}
-
 	conn, err := stan.Connect(config.ClusterID, config.ClientID, config.StanOptions...)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot connect to NATS")
 	}
+	return NewStreamingSubscriberWithStanConn(conn, config.GetStreamingSubscriberSubscriptionConfig(), logger)
+}
 
+func NewStreamingSubscriberWithStanConn(conn stan.Conn, config StreamingSubscriberSubscriptionConfig, logger watermill.LoggerAdapter) (*StreamingSubscriber, error) {
+	if err := config.Validate(); err != nil {
+		return nil, err
+	}
 	return &StreamingSubscriber{
 		conn:    conn,
 		logger:  logger,
