18391182a3040b9c736d21748286403ea39ee2d5
Bartłomiej Kiełbasa <bartlomiej.kielbasa@brainly.com>
2019-05-10 10:49:44 +0200

    exchangeDeclarer to exchangeDeclare

--- message/infrastructure/amqp/topology_builder.go
+++ message/infrastructure/amqp/topology_builder.go
@@ -7,7 +7,7 @@ import (
 )
 
 type TopologyBuilder interface {
-	BuildTopology(channel *amqp.Channel, queueName string, exchangeName string, exchangeDeclarer ExchangeDeclare, config Config, logger watermill.LoggerAdapter) error
+	BuildTopology(channel *amqp.Channel, queueName string, exchangeName string, exchangeDeclare ExchangeDeclare, config Config, logger watermill.LoggerAdapter) error
 }
 
 type ExchangeDeclare func(channel *amqp.Channel, exchangeName string) error
@@ -15,7 +15,7 @@ type ExchangeDeclare func(channel *amqp.Channel, exchangeName string) error
 type DefaultTopologyBuilder struct {
 }
 
-func (builder *DefaultTopologyBuilder) BuildTopology(channel *amqp.Channel, queueName string, exchangeName string, exchangeDeclarer ExchangeDeclare, config Config, logger watermill.LoggerAdapter) error  {
+func (builder *DefaultTopologyBuilder) BuildTopology(channel *amqp.Channel, queueName string, exchangeName string, exchangeDeclare ExchangeDeclare, config Config, logger watermill.LoggerAdapter) error  {
 	if _, err := channel.QueueDeclare(
 		queueName,
 		config.Queue.Durable,
@@ -33,7 +33,7 @@ func (builder *DefaultTopologyBuilder) BuildTopology(channel *amqp.Channel, queu
 		logger.Debug("No exchange to declare", nil)
 		return nil
 	}
-	if err := exchangeDeclarer(channel, exchangeName); err != nil {
+	if err := exchangeDeclare(channel, exchangeName); err != nil {
 		return errors.Wrap(err, "cannot declare exchange")
 	}
 
