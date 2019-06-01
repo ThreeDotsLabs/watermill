fcf68fb4cc35eae247996407ea9aa5246813d426
Maciej Bratek <maclav3@gmail.com>
2019-06-01 13:43:34 +0200

    Update circleci

--- .circleci/wait-for-services.sh
+++ .circleci/wait-for-services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-for service in zookeeper:2181 rabbitmq:5672 googlecloud:8085 nats-streaming:4222 kafka:9092; do
+for service in zookeeper:2181 rabbitmq:5672 googlecloud:8085 nats-streaming:4222 kafka:9092 mysql:3306; do
     "$(dirname "$0")/wait-for-it.sh" -t 60 "$service"
 done
