4c18a1ce2169c218bc64caa54c3d6875f248758c
Maciej Bratek <maclav3@gmail.com>
2019-06-15 01:24:04 +0200

    Improve instant ack godoc

--- message/router/middleware/instant_ack.go
+++ message/router/middleware/instant_ack.go
@@ -3,6 +3,9 @@ package middleware
 import "github.com/ThreeDotsLabs/watermill/message"
 
 // InstantAck makes the handler instantly acknowledge the incoming message, regardless of any errors.
+// It may be used to gain throughput, but at a cost:
+// If you had exactly-once delivery, you may expect at-least-once instead.
+// If you had ordered messages, the ordering might be broken.
 func InstantAck(h message.HandlerFunc) message.HandlerFunc {
 	return func(message *message.Message) ([]*message.Message, error) {
 		message.Ack()
