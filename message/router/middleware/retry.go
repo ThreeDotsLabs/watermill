4538ec1ea4a94403d70a976f5ab9b828091840f7
Maciej Bratek <maclav3@gmail.com>
2019-06-15 12:36:34 +0200

    Update the retry middleware and add docs

--- message/router/middleware/retry.go
+++ message/router/middleware/retry.go
@@ -3,89 +3,88 @@ package middleware
 import (
 	"time"
 
-	"github.com/ThreeDotsLabs/watermill"
+	"github.com/cenkalti/backoff"
 
+	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
 )
-
-type OnRetryHook func(retryNum int, delay time.Duration)
 
 // Retry provides a middleware that retries the handler if errors are returned.
 // The retry behaviour is configurable, with exponential backoff and maximum elapsed time.
 type Retry struct {
+	// MaxRetries is maximum number of times a retry will be attempted.
 	MaxRetries int
 
-	InitialInterval   time.Duration
-	MaxInterval       time.Duration
-	BackOffMultiplier int
-	MaxElapsedTime    time.Duration
+	// InitalInterval is the first interval between retries. Subsequent intervals will be scaled by Multiplier.
+	InitialInterval time.Duration
+	// MaxInterval sets the limit for the exponential backoff of retries. The interval will not be increased beyond MaxInterval.
+	MaxInterval time.Duration
+	// Multiplier is the factor by which the waiting interval will be multiplied between retries.
+	Multiplier float64
+	// MaxElapsedTime sets the time limit of how long retries will be attempted. Disabled if 0.
+	MaxElapsedTime time.Duration
+	// RandomizationFactor randomizes the spread of the backoff times within the interval of:
+	// [currentInterval * (1 - randomization_factor), currentInterval * (1 + randomization_factor)].
+	RandomizationFactor float64
 
-	OnRetryHook OnRetryHook
+	// OnRetryHook is an optional function that will be executed on each retry attempt.
+	// The number of the current retry is passed as retryNum,
+	OnRetryHook func(retryNum int, delay time.Duration)
 
 	Logger watermill.LoggerAdapter
 }
 
 func (r Retry) Middleware(h message.HandlerFunc) message.HandlerFunc {
-	return func(message *message.Message) ([]*message.Message, error) {
-		retries := 0
-		startTime := time.Now()
+	return func(msg *message.Message) ([]*message.Message, error) {
+		producedMessages, err := h(msg)
+		if err == nil {
+			return producedMessages, nil
+		}
 
+		expBackoff := backoff.NewExponentialBackOff()
+		expBackoff.InitialInterval = r.InitialInterval
+		expBackoff.MaxInterval = r.MaxInterval
+		expBackoff.Multiplier = r.Multiplier
+		expBackoff.MaxElapsedTime = r.MaxElapsedTime
+		expBackoff.RandomizationFactor = r.RandomizationFactor
+		ticker := backoff.NewTicker(expBackoff)
+		defer ticker.Stop()
+
+		retryNum := 1
+	retryLoop:
 		for {
-			events, err := h(message)
+			select {
+			case <-msg.Context().Done():
+				return producedMessages, err
+			case _, ok := <-ticker.C:
+				if !ok {
+					break retryLoop
+				}
+			}
+
+			producedMessages, err = h(msg)
 			if err == nil {
-				return events, nil
+				return producedMessages, nil
 			}
 
-			elapsedTime := time.Since(startTime)
-			if !r.shouldRetry(retries, elapsedTime) {
-				return events, err
-			}
-
-			waitTime := r.calculateWaitTime()
 			if r.Logger != nil {
 				r.Logger.Error("Error occurred, retrying", err, watermill.LogFields{
-					"retry_no":     retries,
+					"retry_no":     retryNum,
 					"max_retries":  r.MaxRetries,
-					"wait_time":    waitTime,
-					"elapsed_time": elapsedTime,
+					"wait_time":    expBackoff.NextBackOff(),
+					"elapsed_time": expBackoff.GetElapsedTime(),
 				})
 			}
-			retries++
-
-			select {
-			case <-time.After(waitTime):
-			// ok
-			case <-message.Context().Done():
-				return events, err
+			if r.OnRetryHook != nil {
+				r.OnRetryHook(retryNum, expBackoff.NextBackOff())
 			}
 
-			if r.OnRetryHook != nil {
-				r.OnRetryHook(retries, r.InitialInterval)
+			retryNum++
+			if retryNum > r.MaxRetries {
+				break retryLoop
 			}
 		}
+
+		return nil, err
 	}
-}
-
-func (r Retry) calculateWaitTime() time.Duration {
-	backOffToAdd := r.InitialInterval * time.Duration(r.BackOffMultiplier)
-
-	waitTime := r.InitialInterval + backOffToAdd
-
-	if r.MaxInterval != 0 && waitTime > r.MaxInterval {
-		return r.MaxInterval
-	}
-
-	return waitTime
-}
-
-func (r Retry) shouldRetry(retries int, elapsedTime time.Duration) bool {
-	if r.MaxElapsedTime != 0 && elapsedTime > r.MaxElapsedTime {
-		return false
-	}
-
-	if r.MaxRetries == 0 {
-		return true
-	}
-
-	return retries < r.MaxRetries
 }
