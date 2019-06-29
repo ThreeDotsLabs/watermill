ed7914456099d659d5b7cc9af2d348779d8b6d46
Robert Laszczak <roblaszczak@gmail.com>
2019-06-29 10:35:28 +0200

    templated repository

--- .circleci/wait-for-services.sh
+++ .circleci/wait-for-services.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 
-for service in zookeeper:2181 rabbitmq:5672 googlecloud:8085 nats-streaming:4222 kafka:9092 mysql:3306; do
-    "$(dirname "$0")/wait-for-it.sh" -t 60 "$service"
-done
+# no requirements
