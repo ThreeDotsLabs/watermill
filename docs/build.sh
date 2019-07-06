ed754a57efbd8239b92554c0b0101714cfc197ce
Maciej Bratek <Maciej Bratek>
2019-07-06 21:23:21 +0200

    Templated build.sh with external repositories

--- docs/build.sh
+++ docs/build.sh
@@ -12,70 +12,47 @@ if [[ ! -d themes/kube ]]; then
     popd
 fi
 
+git clone --single-branch --branch master git@github.com:ThreeDotsLabs/watermill-amqp.git content/src-link
+git clone --single-branch --branch master git@github.com:ThreeDotsLabs/watermill-googlecloud.git content/src-link
+git clone --single-branch --branch master git@github.com:ThreeDotsLabs/watermill-http.git content/src-link
+git clone --single-branch --branch master git@github.com:ThreeDotsLabs/watermill-io.git content/src-link
+git clone --single-branch --branch master git@github.com:ThreeDotsLabs/watermill-kafka.git content/src-link
+git clone --single-branch --branch master git@github.com:ThreeDotsLabs/watermill-nats.git content/src-link
+git clone --single-branch --branch master git@github.com:ThreeDotsLabs/watermill-sql.git content/src-link
 
-if [[ "$1" == "--copy" ]]; then
-    rm content/src-link -r || true
-    mkdir content/src-link/
-    cp ../message/ content/src-link/ -r
-    cp ../_examples/ content/src-link/ -r
-    cp ../components/ content/src-link/ -r
-else
-    declare -a files_to_link=(
-        "message/infrastructure/kafka/publisher.go"
-        "message/infrastructure/kafka/subscriber.go"
-        "message/infrastructure/kafka/marshaler.go"
-        "message/infrastructure/kafka/config.go"
-        "message/infrastructure/nats/publisher.go"
-        "message/infrastructure/nats/subscriber.go"
-        "message/infrastructure/nats/marshaler.go"
-        "message/infrastructure/googlecloud/publisher.go"
-        "message/infrastructure/googlecloud/subscriber.go"
-        "message/infrastructure/googlecloud/marshaler.go"
-        "message/infrastructure/gochannel/pubsub.go"
-        "message/infrastructure/http/subscriber.go"
-        "message/infrastructure/http/publisher.go"
-        "message/infrastructure/amqp/doc.go"
-        "message/infrastructure/amqp/publisher.go"
-        "message/infrastructure/amqp/subscriber.go"
-        "message/infrastructure/amqp/config.go"
-        "message/infrastructure/amqp/marshaler.go"
-        "message/infrastructure/amqp/topology_builder.go"
-        "message/infrastructure/io/publisher.go"
-        "message/infrastructure/io/subscriber.go"
-        "message/infrastructure/io/marshal.go"
-        "message/decorator.go"
-        "message/message.go"
-        "message/pubsub.go"
-        "message/router.go"
-        
-        "_examples/cqrs-protobuf/main.go"
-        "components/cqrs/command_bus.go"
-        "components/cqrs/command_processor.go"
-        "components/cqrs/event_bus.go"
-        "components/cqrs/event_processor.go"
-        "components/cqrs/marshaler.go"
-        "components/cqrs/cqrs.go"
-        "components/cqrs/marshaler.go"
 
-        "components/metrics/builder.go"
-        "components/metrics/http.go"
-        "_examples/metrics/main.go"
-    )
 
-    pushd ../
+declare -a files_to_link=(
+    "message/decorator.go"
+    "message/message.go"
+    "message/pubsub.go"
+    "message/router.go"
 
-    for i in "${files_to_link[@]}"
-    do
-        DIR=$(dirname "${i}")
-        DEST_DIR="docs/content/src-link/${DIR}"
+    "_examples/cqrs-protobuf/main.go"
+    "components/cqrs/command_bus.go"
+    "components/cqrs/command_processor.go"
+    "components/cqrs/event_bus.go"
+    "components/cqrs/event_processor.go"
+    "components/cqrs/marshaler.go"
+    "components/cqrs/cqrs.go"
+    "components/cqrs/marshaler.go"
 
-        mkdir -p "${DEST_DIR}"
-        ln -rsf "./${i}" "./${DEST_DIR}"
-    done
+    "components/metrics/builder.go"
+    "components/metrics/http.go"
+    "_examples/metrics/main.go"
+)
 
-    popd
-fi
+pushd ../
 
-python3 ./extract_middleware_godocs.py > content/src-link/middleware-defs.md
+for i in "${files_to_link[@]}"
+do
+    DIR=$(dirname "${i}")
+    DEST_DIR="docs/content/src-link/${DIR}"
+
+    mkdir -p "${DEST_DIR}"
+    ln -rsf "./${i}" "./${DEST_DIR}"
+done
+
+popd
 
 hugo --gc --minify
