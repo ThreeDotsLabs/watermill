43b4d6ed2e9c439553f8661394a9435d064ab046
Michal Sokolowski <michal.sokolowski@karhoo.com>
2019-10-29 21:22:34 +0100

    Improve implementation of duplicator and tests

--- message/router/middleware/duplicator.go
+++ message/router/middleware/duplicator.go
@@ -1,15 +1,36 @@
 package middleware
 
 import (
+	"errors"
+
 	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+var (
+	notIdempotentError = errors.New("handler is not idempotent")
 )
 
 // Duplicator sends message twice.
 func Duplicator(h message.HandlerFunc) message.HandlerFunc {
 	return func(msg *message.Message) ([]*message.Message, error) {
-		events, err := h(msg)
-		_, _ = h(msg)
+		firstProducedMessages, firstErr := h(msg)
+		secondProducedMessages, secondErr := h(msg)
+		allErrorsNil := firstErr == nil && secondErr == nil
+		allErrorsEqual := firstErr != nil && secondErr != nil && firstErr.Error() == secondErr.Error()
 
-		return events, err
+		if allErrorsNil || allErrorsEqual {
+			if len(firstProducedMessages) == len(secondProducedMessages) {
+				for index, msg := range firstProducedMessages {
+					messagesEqual := msg.Equals(secondProducedMessages[index])
+					if !messagesEqual {
+						return nil, notIdempotentError
+					}
+				}
+
+				return firstProducedMessages, nil
+			}
+		}
+
+		return nil, notIdempotentError
 	}
 }
