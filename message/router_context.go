791f4c70ea6c66c3d25b12e5fad2d07880f3ce8a
Robert Laszczak <roblaszczak@gmail.com>
2019-12-08 22:09:14 +0100

    added docs for router context (#167)

--- message/router_context.go
+++ message/router_context.go
@@ -22,22 +22,29 @@ func valFromCtx(ctx context.Context, key ctxKey) string {
 	return val
 }
 
+// HandlerNameFromCtx returns the name of the message handler in the router that consumed the message.
 func HandlerNameFromCtx(ctx context.Context) string {
 	return valFromCtx(ctx, handlerNameKey)
 }
 
+// PublisherNameFromCtx returns the name of the message publisher type that published the message in the router.
+// For example, for Kafka it will be `kafka.Publisher`.
 func PublisherNameFromCtx(ctx context.Context) string {
 	return valFromCtx(ctx, publisherNameKey)
 }
 
+// SubscriberNameFromCtx returns the name of the message subscriber type that subscribed to the message in the router.
+// For example, for Kafka it will be `kafka.Subscriber`.
 func SubscriberNameFromCtx(ctx context.Context) string {
 	return valFromCtx(ctx, subscriberNameKey)
 }
 
+// SubscribeTopicFromCtx returns the topic from which message was received in the router.
 func SubscribeTopicFromCtx(ctx context.Context) string {
 	return valFromCtx(ctx, subscribeTopicKey)
 }
 
+// PublishTopicFromCtx returns the topic to which message will be published by the router.
 func PublishTopicFromCtx(ctx context.Context) string {
 	return valFromCtx(ctx, publishTopicKey)
 }
