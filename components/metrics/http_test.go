91881b1a563e7b29c1206ed3a3aa742667eebb86
Baptiste Lefrancois <baptiste.lefrancois@ovoenergy.com>
2020-03-20 01:41:19 +0000

    Wait for metrics server to be ready in tests

--- components/metrics/http_test.go
+++ components/metrics/http_test.go
@@ -3,6 +3,7 @@ package metrics_test
 import (
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/ThreeDotsLabs/watermill/components/metrics"
 	"github.com/pkg/errors"
@@ -17,7 +18,7 @@ func TestCreateRegistryAndServeHTTP_metrics_endpoint(t *testing.T) {
 	if err != nil {
 		t.Fatal(errors.Wrap(err, "registration of prometheus build info collector failed"))
 	}
-
+	waitServerReady(t, "http://localhost:8090")
 	resp, err := http.DefaultClient.Get("http://localhost:8090/metrics")
 	if resp != nil {
 		defer resp.Body.Close()
@@ -37,7 +38,7 @@ func TestCreateRegistryAndServeHTTP_unknown_endpoint(t *testing.T) {
 	if err != nil {
 		t.Error(errors.Wrap(err, "registration of prometheus build info collector failed"))
 	}
-
+	waitServerReady(t, "http://localhost:8091")
 	resp, err := http.DefaultClient.Get("http://localhost:8091/unknown")
 	if resp != nil {
 		defer resp.Body.Close()
@@ -48,4 +49,17 @@ func TestCreateRegistryAndServeHTTP_unknown_endpoint(t *testing.T) {
 	}
 	assert.NotNil(t, resp)
 	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+}
+
+// server might have small delay before being able to server traffic
+func waitServerReady(t *testing.T, addr string) {
+	for i := 0; i < 50; i++ {
+		_, err := http.DefaultClient.Get(addr)
+		// assume server ready when no err anymore
+		if err == nil {
+			return
+		}
+		time.Sleep(100 * time.Millisecond)
+		continue
+	}
 }
