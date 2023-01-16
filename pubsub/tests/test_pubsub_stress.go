f6254b4745a61ac799c4776426f49049224b8336
Miłosz Smółka <milosz.smolka@gmail.com>
2023-01-16 23:12:08 +0100

    Stress tests: set GOMAXPROCS to double the number of CPUs (#320)

--- pubsub/tests/test_pubsub_stress.go
+++ pubsub/tests/test_pubsub_stress.go
@@ -3,7 +3,14 @@
 
 package tests
 
+import (
+	"runtime"
+)
+
 func init() {
 	// stress tests may work a bit slower
 	defaultTimeout *= 5
+
+	// Set GOMAXPROCS to double the number of CPUs
+	runtime.GOMAXPROCS(runtime.GOMAXPROCS(0) * 2)
 }
