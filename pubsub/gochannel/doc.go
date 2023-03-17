0ee2c7acf52ae430fbb66c8de694740215e6e58c
tjnet <tjnet@users.noreply.github.com>
2023-03-17 16:51:29 +0900

    correct link of doc about pub/sub implementation supported by watermill (#357)

--- pubsub/gochannel/doc.go
+++ pubsub/gochannel/doc.go
@@ -1,5 +1,5 @@
 // This is just the simplest Pub/Sub implementation
 //
-// All Pub/Sub implementations can be found at https://watermill.io/docs/pub-sub-implementations/
+// All Pub/Sub implementations can be found at https://watermill.io/pubsubs/
 
 package gochannel
