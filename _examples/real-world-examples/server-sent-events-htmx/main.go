32e268ff0cc449faeb228d99977498c91e77eecf
Miłosz Smółka <milosz.smolka@gmail.com>
2024-04-16 13:39:13 +0200

    Updated example

--- _examples/real-world-examples/server-sent-events-htmx/main.go
+++ _examples/real-world-examples/server-sent-events-htmx/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"embed"
-	"encoding/json"
 	"fmt"
+	"html/template"
 	stdHttp "net/http"
-	"time"
 
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-http/v2/pkg/http"
@@ -16,8 +16,44 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+var messages = map[string]string{
+	"a": "🤩",
+	"b": "⛄️",
+	"c": "🌝",
+	"d": "🙈",
+}
+
+var teams = map[string]Team{
+	"a": {
+		ID:    "a",
+		Color: "danger",
+	},
+	"b": {
+		ID:    "b",
+		Color: "warning",
+	},
+	"c": {
+		ID:    "c",
+		Color: "primary",
+	},
+	"d": {
+		ID:    "d",
+		Color: "success",
+	},
+}
+
+type Team struct {
+	ID      string
+	Color   string
+	Count   int
+	Percent int
+}
+
 //go:embed public/*.html
 var staticFS embed.FS
+
+//go:embed templates/*.html
+var templatesFS embed.FS
 
 func main() {
 	logger := watermill.NewStdLogger(false, false)
@@ -31,75 +67,82 @@ func main() {
 		panic(err)
 	}
 
-	go func() {
-		count := 0
-		for {
-			time.Sleep(5 * time.Second)
-			count++
-			if count > 99 {
-				count = 0
-			}
-
-			payload := notifications{
-				Count: count,
-			}
-			bytes, err := json.Marshal(payload)
-			if err != nil {
-				panic(err)
-			}
-
-			msg := message.NewMessage(watermill.NewUUID(), bytes)
-			err = pubSub.Publish("notifications", msg)
-			if err != nil {
-				panic(err)
-			}
-		}
-	}()
-
-	go func() {
-		var messages []string
-		for {
-			time.Sleep(5 * time.Second)
-			messages = append(messages, "Hello, world!")
-			if len(messages) > 5 {
-				messages = nil
-			}
-
-			row := `
-<div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
-  <div class="toast-header">
-    <strong class="me-auto">Bootstrap</strong>
-    <small>11 mins ago</small>
-  </div>
-  <div class="toast-body">%s</div>
-</div>
-`
-
-			var payload string
-
-			for _, message := range messages {
-				payload += fmt.Sprintf(row, message)
-			}
-
-			msg := message.NewMessage(watermill.NewUUID(), []byte(payload))
-			err := pubSub.Publish("messages", msg)
-			if err != nil {
-				panic(err)
-			}
-		}
-	}()
-
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 
 	e.StaticFS("/", echo.MustSubFS(staticFS, "public"))
 
-	notificationsHandler := sseRouter.AddHandler("notifications", notificationsStreamAdapter{})
+	teamsHandler := sseRouter.AddHandler("teams-updated", teamsStreamAdapter{})
 	messagesHandler := sseRouter.AddHandler("messages", messagesStreamAdapter{})
 
-	e.GET("/api/notifications", echo.WrapHandler(notificationsHandler))
+	e.GET("/api/teams", echo.WrapHandler(teamsHandler))
 	e.GET("/api/messages", echo.WrapHandler(messagesHandler))
+
+	e.POST("/api/join", func(c echo.Context) error {
+		teamID := c.FormValue("team")
+
+		team, ok := teams[teamID]
+		if !ok {
+			return c.String(stdHttp.StatusNotFound, "team not found")
+		}
+
+		team.Count++
+		teams[teamID] = team
+
+		var allCount int
+		var keys []string
+		for _, id := range []string{"a", "b", "c", "d"} {
+			t := teams[id]
+			if t.Count > 0 {
+				allCount += t.Count
+				keys = append(keys, t.ID)
+			}
+		}
+
+		if len(keys) == 1 {
+			t := teams[keys[0]]
+			t.Percent = 100
+			teams[keys[0]] = t
+		} else if len(keys) > 1 {
+			remainingPercent := 100
+			for _, id := range keys[:len(keys)-1] {
+				t := teams[id]
+				t.Percent = int(float64(t.Count) / float64(allCount) * 100)
+				remainingPercent -= t.Percent
+				teams[id] = t
+			}
+
+			t := teams[keys[len(keys)-1]]
+			t.Percent = remainingPercent
+			teams[keys[len(keys)-1]] = t
+		}
+
+		msg := message.NewMessage(watermill.NewUUID(), []byte(nil))
+		err := pubSub.Publish("teams-updated", msg)
+		if err != nil {
+			return c.String(stdHttp.StatusInternalServerError, err.Error())
+		}
+
+		return c.NoContent(stdHttp.StatusAccepted)
+	})
+
+	e.PUT("/api/messages/:message", func(c echo.Context) error {
+		messageID := c.Param("message")
+
+		_, ok := messages[messageID]
+		if !ok {
+			return c.String(stdHttp.StatusNotFound, "message not found")
+		}
+
+		msg := message.NewMessage(watermill.NewUUID(), []byte(messageID))
+		err := pubSub.Publish("messages", msg)
+		if err != nil {
+			return c.String(stdHttp.StatusInternalServerError, err.Error())
+		}
+
+		return c.NoContent(stdHttp.StatusAccepted)
+	})
 
 	go func() {
 		err := sseRouter.Run(context.Background())
@@ -114,24 +157,27 @@ func main() {
 	}
 }
 
-type notifications struct {
-	Count int `json:"count"`
+type teamsStreamAdapter struct{}
+
+func (t teamsStreamAdapter) InitialStreamResponse(w stdHttp.ResponseWriter, r *stdHttp.Request) (response interface{}, ok bool) {
+	return t.getResponse()
 }
 
-type notificationsStreamAdapter struct{}
-
-func (n notificationsStreamAdapter) InitialStreamResponse(w stdHttp.ResponseWriter, r *stdHttp.Request) (response interface{}, ok bool) {
-	return 0, true
+func (t teamsStreamAdapter) NextStreamResponse(r *stdHttp.Request, msg *message.Message) (response interface{}, ok bool) {
+	return t.getResponse()
 }
 
-func (n notificationsStreamAdapter) NextStreamResponse(r *stdHttp.Request, msg *message.Message) (response interface{}, ok bool) {
-	var payload notifications
-	err := json.Unmarshal(msg.Payload, &payload)
+func (t teamsStreamAdapter) getResponse() (string, bool) {
+	tmpl := template.Must(template.ParseFS(templatesFS, "templates/teams.html"))
+
+	var buffer bytes.Buffer
+	err := tmpl.Execute(&buffer, teams)
 	if err != nil {
-		return nil, false
+		fmt.Println(err)
+		return "", false
 	}
 
-	return payload.Count, true
+	return buffer.String(), true
 }
 
 type messagesStreamAdapter struct{}
@@ -141,5 +187,9 @@ func (m messagesStreamAdapter) InitialStreamResponse(w stdHttp.ResponseWriter, r
 }
 
 func (m messagesStreamAdapter) NextStreamResponse(r *stdHttp.Request, msg *message.Message) (response interface{}, ok bool) {
-	return string(msg.Payload), true
+	text, ok := messages[string(msg.Payload)]
+	if !ok {
+		return "", false
+	}
+	return fmt.Sprintf("<p>%v</p>", text), true
 }
