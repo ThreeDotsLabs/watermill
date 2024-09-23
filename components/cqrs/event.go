3fc71897f3d126c8e58cef13f41faf2202a6f8cf
Miłosz Smółka <milosz.smolka@gmail.com>
2024-09-23 11:40:04 +0200

    Add Event interface and GenerateEventName

---
+++ components/cqrs/event.go
@@ -0,0 +1,14 @@
+package cqrs
+
+// Event is an interface that describes an event.
+type Event interface {
+    // EventName should return the name of the event. For example, it could be used for topic generation.
+	EventName() string
+}
+
+// GenerateEventName returns the Event's name.
+// It can be used with marshalers if events implement the Event interface.
+func GenerateEventName(v any) string {
+	e := v.(Event)
+	return e.EventName()
+}
