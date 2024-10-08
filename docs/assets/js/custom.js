cf8326e3e70fcc63b2e0253ef5f846ba29b827b4
Robert Laszczak <roblaszczak@gmail.com>
2024-10-08 18:32:04 +0200

    - dark mode by default + working toggle - better fonts support - adjust colors - og:image with SSR

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -1,1 +1,7 @@
 // Put your custom JS code here
+
+// a bit hacky way to force dark mode by default
+// it sets local storage item used by docs/node_modules/@thulite/doks-core/assets/js/color-mode.js
+if (!localStorage.getItem('theme')) {
+  localStorage.setItem('theme', 'dark');
+}
