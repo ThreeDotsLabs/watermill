8b69ce2ed5686ffef611ecf1c1c30f3756e5fd06
Robert Laszczak <roblaszczak@gmail.com>
2024-10-08 19:21:14 +0200

    added stars info

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -5,3 +5,32 @@
 if (!localStorage.getItem('theme')) {
   localStorage.setItem('theme', 'dark');
 }
+
+import { render } from 'github-buttons';
+
+let renderGitHubButton= () => {
+  let oldButton = document.getElementById("github-button");
+  if (oldButton) {
+    oldButton.remove();
+  }
+
+  let options = {
+    "href": "https://github.com/ThreeDotsLabs/watermill",
+    "data-show-count": true,
+    "data-size": "large",
+    "data-color-scheme": localStorage.getItem('theme'),
+  }
+
+  render(options, function (el) {
+    let menu = document.getElementById("offcanvasNavMain").querySelector(".offcanvas-body");
+    let searchToggle = document.getElementById("searchToggleDesktop");
+
+    el.setAttribute("id", "github-button");
+    el.classList.add("nav-link", "px-2", "mx-auto");
+    el.setAttribute("style", "margin-top: 12px;");
+
+
+    menu.insertBefore(el, searchToggle);
+  })
+}
+renderGitHubButton()
