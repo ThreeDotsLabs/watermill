b75dea75101d6afb298f99509d7d804a61b90c19
ahkui <ahkui@outlook.com>
2026-07-29 21:31:50 +0800

    feat(opentelemetry): move opentelemetry metrics to a separate module
    Signed-off-by: ahkui <ahkui@outlook.com>

---
+++ components/opentelemetry/labels.go
@@ -0,0 +1,46 @@
+package opentelemetry
+
+import (
+	"context"
+
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+const (
+	labelKeyHandlerName    = "handler_name"
+	labelKeyPublisherName  = "publisher_name"
+	labelKeySubscriberName = "subscriber_name"
+	labelSuccess           = "success"
+	labelAcked             = "acked"
+
+	labelValueNoHandler = "<no handler>"
+)
+
+var (
+	labelGetters = map[string]func(context.Context) string{
+		labelKeyHandlerName:    message.HandlerNameFromCtx,
+		labelKeyPublisherName:  message.PublisherNameFromCtx,
+		labelKeySubscriberName: message.SubscriberNameFromCtx,
+	}
+)
+
+func labelsFromCtx(ctx context.Context, labels ...string) map[string]string {
+	ctxLabels := map[string]string{}
+
+	for _, l := range labels {
+		k := l
+		ctxLabels[l] = ""
+
+		getter, ok := labelGetters[k]
+		if !ok {
+			continue
+		}
+
+		v := getter(ctx)
+		if v != "" {
+			ctxLabels[l] = v
+		}
+	}
+
+	return ctxLabels
+}
